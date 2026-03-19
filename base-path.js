316cf971a5ad4a9b40383777b19e573d9f14ce90
timajan07 <timajan07@gmail.com>
2026-03-19 09:37:18 +0200

    chore: added lab 1

--- base-path.js
+++ base-path.js
@@ -1,3 +1,3 @@
 export const BASE_PATH =
   process.env.BASE_PATH ??
-  (process.env.NODE_ENV === "production" ? "/Labs-Markdown-Template" : "");
+  (process.env.NODE_ENV === "production" ? "/IK-33_appRECORD-MaistrenkoTimothy-FIOT-2026" : "");
